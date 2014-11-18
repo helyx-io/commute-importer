7997a9f5ea478b0ff02d36ffcf4b2c87a710c5ce
Alexis Kinsella <alexis.kinsella@gmail.com>
2014-11-18 01:02:58 +0100

    Added login page

--- controller/authCtrl.go
+++ controller/authCtrl.go
@@ -33,13 +33,13 @@ type AuthController struct { }
 func (ac *AuthController) Init(r *mux.Router) {
 
 	// Init Router
-	r.HandleFunc("/", ac.Login)
-	r.HandleFunc("/callback", ac.AuthCallback)
+	r.HandleFunc("/google", ac.AuthGoogle)
+	r.HandleFunc("/google/callback", ac.AuthGoogleCallback)
 
 	client = &oauth.Config{
 		ClientId:     config.OAuthInfos.ClientId,
 		ClientSecret: config.OAuthInfos.ClientSecret,
-		RedirectURL:  "http://localhost:3000/auth/callback",
+		RedirectURL:  "http://localhost:3000/auth/google/callback",
 		Scope:		  "https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile",
 		AuthURL:      "https://accounts.google.com/o/oauth2/auth",
 		TokenURL:     "https://accounts.google.com/o/oauth2/token",
@@ -49,14 +49,11 @@ func (ac *AuthController) Init(r *mux.Router) {
 }
 
 
-func (ac *AuthController) Login(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<html><body>"))
-	//w.Write([]byte(fmt.Sprintf("<a href=\"/authorize?response_type=code&client_id=1234&state=xyz&scope=everything&redirect_uri=%s\">Login</a><br/>", url.QueryEscape("http://localhost:14000/appauth/code"))))
-	w.Write([]byte(fmt.Sprintf("<a href=\"%s\">Login</a><br/>", client.AuthCodeURL(""))))
-	w.Write([]byte("</body></html>"))
+func (ac *AuthController) AuthGoogle(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, client.AuthCodeURL(""), 302)
 }
 
-func (ac *AuthController) AuthCallback(w http.ResponseWriter, r *http.Request) {
+func (ac *AuthController) AuthGoogleCallback(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
 	code := r.Form.Get("code")
@@ -70,12 +67,10 @@ func (ac *AuthController) AuthCallback(w http.ResponseWriter, r *http.Request) {
 		var err error
 
 		// if parse, download and parse json
-		if r.Form.Get("doparse") == "1" {
-			jr, err = ctransport.Exchange(code)
-			if err != nil {
-				jr = nil
-				w.Write([]byte(fmt.Sprintf("ERROR: %s<br/>\n", err)))
-			}
+		jr, err = ctransport.Exchange(code)
+		if err != nil {
+			jr = nil
+			w.Write([]byte(fmt.Sprintf("ERROR: %s<br/>\n", err)))
 		}
 
 		// show json access token
@@ -87,12 +82,6 @@ func (ac *AuthController) AuthCallback(w http.ResponseWriter, r *http.Request) {
 		}
 
 		w.Write([]byte(fmt.Sprintf("FULL RESULT: %+v<br/>\n", jr)))
-
-		cururl := *r.URL
-		curq := cururl.Query()
-		curq.Add("doparse", "1")
-		cururl.RawQuery = curq.Encode()
-		w.Write([]byte(fmt.Sprintf("<a href=\"%s\">Download Token</a><br/>", cururl.String())))
 	} else {
 		w.Write([]byte("Nothing to do"))
 	}
