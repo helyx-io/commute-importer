6226a3058d3ceb1e38f4da6fa1d17e9cf6fe4f70
Alexis Kinsella <alexis.kinsella@gmail.com>
2015-01-24 18:39:21 +0100

    Added new agencies

--- config/config.go
+++ config/config.go
@@ -120,10 +120,106 @@ func Init() error {
 
 	DataResources = make(map[string]string)
 
-	DataResources["PARIS_GTFS_20140502"] = BaseURL + "/PARIS_GTFS_20140502.zip"
-	DataResources["RATP_GTFS_FULL"] = BaseURL + "/RATP_GTFS_FULL.zip"
-	DataResources["RATP_GTFS_LINES"] = BaseURL + "/RATP_GTFS_LINES.zip"
-	DataResources["STAR_GTFS_RENNES"] = BaseURL + "/STAR_GTFS_RENNES.zip"
+
+	/**
+	 * Région: Ile-de-France
+	 * Agence: RATP
+	 * Site: http://data.ratp.fr/fr/les-donnees/fiche-de-jeu-de-donnees/dataset/offre-transport-de-la-ratp-format-gtfs.html?tx_icsoddatastore_pi1%5BreturnID%5D=38
+	 **/
+	DataResources["RATP"] = "http://dataratp.download.opendatasoft.com/RATP_GTFS_FULL.zip"
+
+	/**
+	 * Région: Ile-de-France
+	 * Agence: INTERCITES
+	 * Site: https://ressources.data.sncf.com/explore/dataset/sncf-intercites-gtfs/?tab=metas
+	 **/
+	DataResources["INTERCITES"] = "http://ressources.data.sncf.com/api/datasets/1.0/sncf-intercites-gtfs/attachments/export_intercites_gtfs_last_zip/"
+
+	/**
+	 * Région: Ile-de-France
+	 * Agence: TER
+	 * Site: https://ressources.data.sncf.com/explore/dataset/sncf-ter-gtfs/?tab=metas
+	 **/
+	DataResources["TER"] = "http://ressources.data.sncf.com/api/datasets/1.0/sncf-ter-gtfs/attachments/export_ter_gtfs_last_zip/"
+
+	/**
+	 * Région: Ile-de-France
+	 * Agence: Transilien
+	 * Site: https://ressources.data.sncf.com/explore/dataset/sncf-transilien-gtfs/?tab=metas
+	 **/
+	DataResources["TRANSILIEN"] = "http://ressources.data.sncf.com/api/datasets/1.0/sncf-transilien-gtfs/attachments/export_tn_gtfs_last_zip/"
+
+	/**
+	 * Région: REnnes
+	 * Agence: STAR (Keolis)
+	 * Site: http://data.keolis-rennes.com/fr/les-donnees/donnees-telechargeables.html
+	 **/
+	DataResources["STAR"] = "http://data.keolis-rennes.com/fileadmin/OpenDataFiles/GTFS/GTFS-20150120.zip"
+
+	/**
+	 * Ville: Nantes
+	 * Agence: SEMITAN
+	 * Site: http://data.nantes.fr/donnees/detail/arrets-horaires-et-circuits-tan/
+	 **/
+	DataResources["SEMITAN"] = "http://data.nantes.fr/fileadmin/data/datastore/nm/mobilite/24440040400129_NM_TAN_00005/ARRETS_HORAIRES_CIRCUITS_TAN_gtfs.zip"
+
+	/**
+	 * Département: Loires-Atlantique
+	 * Agence: LILA
+	 * Site: http://data.loire-atlantique.fr/donnees/detail/horaires-et-points-darrets-du-reseau-de-transport-lila-lignes-regulieres/
+	 **/
+	DataResources["LILA"] = "http://data.loire-atlantique.fr/fileadmin/data/datastore/cg44/mobilite/22440002800011_CG44_MOB_09003/MOB09003_gtfs.zip"
+
+	/**
+	 * Ville: Toulouse
+	 * Agence: Tisseo
+	 * Site: http://data.toulouse-metropole.fr/les-donnees/-/opendata/card/16271-reseau-tisseo-metro-bus-tram-gtfs
+	 **/
+	DataResources["TISSEO"] = "http://data.toulouse-metropole.fr/les-donnees/-/opendata/card/16271-reseau-tisseo-metro-bus-tram-gtfs/resource/document?p_p_state=exclusive&_5_WAR_opendataportlet_jspPage=%2Fsearch%2Fview_card.jsp"
+
+	/**
+	 * Ville: Lille
+	 * Agence: Transpole
+	 * Site: http://www.transpole.fr/
+	 * Url: https://twitter.com/schignard/status/509780362785218560
+	 **/
+	DataResources["LILLE"] = "https://www.data.gouv.fr/_uploads/resources/GTFS-Lille-20140801_1.zip"
+
+	/**
+	 * Région: Gironde
+	 * Agence: Transgironde
+	 * Site: http://transgironde.gironde.fr/
+	 **/
+	DataResources["TRANSGIRONDE"] = "http://catalogue.datalocale.fr/storage/f/2013-03-19T174734/ExportGTFS20130319.zip"
+
+	/**
+	 * Région: Gironde
+	 * Agence: Cub
+	 * Site: http://www.infotbc.com/
+	 **/
+	DataResources["CUB"] = "http://data.lacub.fr/files.php?gid=67&format=14"
+
+	/**
+	 * Ville: Metz
+	 * Agence: LeMet / Mettis
+	 * Site: https://github.com/ridem/fr.lemet/tree/master/TransportsMetzHelper/src/fr/ybo/transportsrenneshelper/gtfs
+	 **/
+	DataResources["METZ"] = "https://geo-ws.metzmetropole.fr/services/opendata/gtfs_hiver_nov14_031114-050715.zip"
+
+	/**
+	 * Ville: Angers
+	 * Agence: Irigo
+	 * Site: http://data.angers.fr/donnees/mobilite-horaire-de-passage-theorique/
+	 **/
+	DataResources["IRIGO"] = "http://data.angers.fr/?eID=ics_od_datastoredownload&file=824"
+
+	/**
+	 * Ville: Nancy
+	 * Agence: STAN
+	 * Site: http://opendata.grand-nancy.org/
+	 **/
+	DataResources["STAN"] = "http://opendata.grand-nancy.org/?eID=ics_od_datastoredownload&file=333"
+
 
 	httpPort, _ := strconv.Atoi(os.Getenv("HTTP_PORT"))
 	if httpPort == 0 {
