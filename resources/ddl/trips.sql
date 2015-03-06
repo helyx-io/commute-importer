a8f6b55a51b505ea5813eab55146435249873250
Alexis Kinsella <alexis.kinsella@gmail.com>
2015-03-07 00:59:47 +0100

    trip_headsign field was too short

--- resources/ddl/trips.sql
+++ resources/ddl/trips.sql
@@ -2,7 +2,7 @@ CREATE TABLE `gtfs_%s`.`trips` (
   `route_id` int(11) NOT NULL,
   `service_id` int(11) NOT NULL,
   `trip_id` int(11) NOT NULL,
-  `trip_headsign` char(8) DEFAULT NULL,
+  `trip_headsign` varchar(45) DEFAULT NULL,
   `direction_id` int(11) DEFAULT NULL,
   `block_id` int(11) DEFAULT NULL,
   `shape_id` int(11) DEFAULT NULL,
