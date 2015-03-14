31cb0b0be35134139695acfa338604dc7ca2af63
Alexis Kinsella <alexis.kinsella@gmail.com>
2015-03-15 00:16:05 +0100

    Reimplemented nearest agency and stop apis used in commute webapp

--- controller/agencyCtrl.go
+++ controller/agencyCtrl.go
@@ -9,6 +9,7 @@ import (
 	"log"
     "sync"
     "time"
+    "sort"
     "net/http"
     "encoding/json"
     "gopkg.in/redis.v2"
@@ -23,16 +24,46 @@ import (
 /// Structures
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
+type Stops []Stop
+
 type Stop struct {
     Id int
     Name string
+    Desc string
+    Lat string
+    Lon string
+    LocationType int
     Distance float64
-    Routes []Route
+    Routes Routes
 }
+
+type StopGroups []StopGroup
+
+type StopGroup struct {
+    Ids []int
+    Name string
+    Desc string
+    Lat string
+    Lon string
+    LocationType int
+    Distance float64
+    Routes Routes
+}
+
+type StopGroupByDistance []StopGroup
+func (sgbd StopGroupByDistance) Len() int { return len(sgbd) }
+func (sgbd StopGroupByDistance) Swap(i, j int) { sgbd[i], sgbd[j] = sgbd[j], sgbd[i] }
+func (sgbd StopGroupByDistance) Less(i, j int) bool { return sgbd[i].Distance < sgbd[j].Distance }
+
+
+type Routes []Route
 
 type Route struct {
     Name string
     TripId int
+    RouteType int
+    RouteColor string
+    RouteTextColor string
     FirstStopName string
     LastStopName string
     StopTimesFull []StopTimeFull
@@ -56,19 +87,60 @@ type StopTimeFull struct {
     TripId int
 }
 
+type Agencies []Agency
+
+type Agency struct {
+    Key string
+    Id int
+    Name string
+    Url string
+    Timezone string
+    Lang string
+    MinLat float64
+    MaxLat float64
+    MinLon float64
+    MaxLon float64
+}
+
+
+type JsonAgency struct {
+    Key string `json:"key"`
+    Id int `json:"id"`
+    Name string `json:"name"`
+    Url string `json:"url"`
+    Timezone string `json:"timezone"`
+    Lang string `json:"lang"`
+    MinLat float64 `json:"min_lat"`
+    MaxLat float64 `json:"max_lat"`
+    MinLon float64 `json:"min_lon"`
+    MaxLon float64 `json:"max_lon"`
+}
+
+
+type StopTimeFullByDepartureDate []StopTimeFull
+func (stf StopTimeFullByDepartureDate) Len() int { return len(stf) }
+func (stf StopTimeFullByDepartureDate) Swap(i, j int) { stf[i], stf[j] = stf[j], stf[i] }
+func (stf StopTimeFullByDepartureDate) Less(i, j int) bool { return stf[i].DepartureTime < stf[j].DepartureTime }
+
+type RouteByName []Route
+func (rn RouteByName) Len() int { return len(rn) }
+func (rn RouteByName) Swap(i, j int) { rn[i], rn[j] = rn[j], rn[i] }
+func (rn RouteByName) Less(i, j int) bool { return rn[i].Name < rn[j].Name }
+
+
 type FirstLastStopNamesByTripId struct {
     TripId int
     FirstStopName string
     LastStopName string
 }
 
-type JsonStop struct {
+type JsonStopGroup struct {
+    Ids []int `json:"ids"`
     Name string `json:"name"`
     Desc string `json:"desc"`
-    Distance string `json:"distance"`
+    Distance float64 `json:"distance"`
     LocationType int `json:"location_type"`
     GeoLocation JsonGeoLocation `json:"geo_location"`
-    StopIds []string `json:"stop_ids"`
     Routes []JsonRoute `json:"routes"`
 }
 
@@ -78,14 +150,14 @@ type JsonGeoLocation struct {
 }
 
 type JsonRoute struct {
-    Name string `json:"lat"`
-    StopTimes []string `json:"stop_times"`
+    Name string `json:"name"`
     TripId int `json:"trip_id"`
+    RouteType int `json:"route_type"`
     RouteColor string `json:"route_color"`
     RouteTextColor string `json:"route_text_color"`
-    RouteType string `json:"route_type"`
     FirstStopName string `json:"first_stop_name"`
     LastStopName string `json:"last_stop_name"`
+    StopTimes []string `json:"stop_times"`
 }
 
 
@@ -113,7 +185,8 @@ func (ac *AgencyController) Init(r *mux.Router) {
     })
 
     // Init Router
-	r.HandleFunc("/{agencyKey}/stops/{date}/nearest", ac.NearestStops)
+    r.HandleFunc("/{agencyKey}/stops/{date}/nearest", ac.NearestStops)
+    r.HandleFunc("/nearest", ac.NearestAgencies)
 }
 
 
@@ -149,28 +222,145 @@ func (ac *AgencyController) NearestStops(w http.ResponseWriter, r *http.Request)
     log.Printf("Extracting Trip Ids ...")
     tripIds := extractTripIds(stops)
 
-
     log.Printf("Fetching First And Last StopNames By Trip Ids ...")
     flStopNamesByTripId := fetchFirstAndLastStopNamesByTripIds(agencyKey, tripIds)
 
     log.Printf("Merge First and Last StopNames By TripId With Stop Routes ...")
-    mergeFlStopNamesByTripIdWithStopRoutes(stops, flStopNamesByTripId)
-
-    log.Printf("Resulting flStopNamesByTripId: %v", flStopNamesByTripId)
+    mergeFlStopNamesByTripIdWithStopRoutes(&stops, flStopNamesByTripId)
 
     log.Printf("-----------------------------------------------------------------------------------")
-    log.Printf("--- NearestStops. ElapsedTime: %v", sw.ElapsedTime())
+    log.Printf("--- Nearest stops. ElapsedTime: %v", sw.ElapsedTime())
     log.Printf("-----------------------------------------------------------------------------------")
 
 
     w.Header().Set("X-Response-Time", sw.ElapsedTime().String())
-    w.Write([]byte(fmt.Sprintf("ElapsedTime: %v", sw.ElapsedTime())))
+
+    stopGroups := stops.toStopGroups()
+    sort.Sort(StopGroupByDistance(stopGroups))
+
+    jsm, err := json.Marshal(stopGroups.toJsonStopGroups())
+
+    utils.FailOnError(err, "Failed on marshalling json stops")
+    w.Write(jsm)
 }
 
-func mergeFlStopNamesByTripIdWithStopRoutes(stops []Stop, flStopNamesByTripId map[int]FirstLastStopNamesByTripId) {
 
-    for _, stop := range stops {
-        for _, route := range stop.Routes {
+func (ac *AgencyController) NearestAgencies(w http.ResponseWriter, r *http.Request) {
+
+    defer utils.RecoverFromError(w)
+
+    sw := stopwatch.Start(0)
+
+    lat := r.URL.Query().Get("lat")
+    lon := r.URL.Query().Get("lon")
+
+    log.Printf("Lat: %s", lat)
+    log.Printf("Lon: %s", lon)
+
+    log.Printf("-----------------------------------------------------------------------------------")
+    log.Printf("--- Nearest agencies. ElapsedTime: %v", sw.ElapsedTime())
+    log.Printf("-----------------------------------------------------------------------------------")
+
+    w.Header().Set("X-Response-Time", sw.ElapsedTime().String())
+
+    agencies := fetchNearestAgencies(lat, lon)
+
+    jsm, err := json.Marshal(agencies.toJsonAgencies())
+
+    utils.FailOnError(err, "Failed on marshalling json agencies")
+    w.Write(jsm)
+}
+
+
+func (ss Stops) toStopGroups() StopGroups {
+    sgsByKey := make(map[string]StopGroup)
+
+    for _, s := range ss {
+        key := fmt.Sprintf("%s%s%d", s.Name, s.Desc, s.LocationType)
+
+        if sg, ok := sgsByKey[key]; !ok {
+            sgsByKey[key] = s.toStopGroup()
+        } else {
+            sg.Ids = append(sg.Ids, s.Id)
+            for _, r := range s.Routes {
+                sg.Routes = append(sg.Routes, r)
+            }
+
+            sort.Sort(RouteByName(sg.Routes))
+
+            sgsByKey[key] = sg
+        }
+    }
+    sgs := make(StopGroups, 0)
+
+    for _, value := range sgsByKey {
+//        log.Printf("Key: %s - Value: %v", key, value)
+        sgs = append(sgs, value)
+    }
+
+    return sgs
+}
+
+func (s Stop) toStopGroup() StopGroup {
+    return StopGroup{[]int{s.Id}, s.Name, s.Desc, s.Lat, s.Lon, s.LocationType, s.Distance, s.Routes}
+}
+
+
+func (sgs StopGroups) toJsonStopGroups() []JsonStopGroup {
+    jsgs := make([]JsonStopGroup, len(sgs))
+
+    for i, sg := range sgs {
+        jsgs[i] = sg.toJsonStopGroup()
+//        log.Printf("Index: %d - json: %v", i, jsgs[i])
+    }
+
+    return jsgs
+}
+
+func (rs Routes) toJsonRoutes() []JsonRoute {
+    jrs := make([]JsonRoute, len(rs))
+
+    for i, r := range rs {
+        jrs[i] = r.toJsonRoute()
+    }
+
+    return jrs
+}
+
+func (sg StopGroup) toJsonStopGroup() JsonStopGroup {
+    return JsonStopGroup{sg.Ids, sg.Name, sg.Desc, sg.Distance, sg.LocationType, JsonGeoLocation{sg.Lat, sg.Lon}, sg.Routes.toJsonRoutes()}
+}
+
+func (as Agencies) toJsonAgencies() []JsonAgency {
+    jas := make([]JsonAgency, len(as))
+
+    for i, a := range as {
+        jas[i] = a.toJsonAgency()
+    }
+
+    return jas
+}
+
+func (a Agency) toJsonAgency() JsonAgency {
+    return JsonAgency{a.Key, a.Id, a.Name, a.Url, a.Timezone, a.Lang, a.MinLat, a.MaxLat, a.MinLon, a.MaxLon}
+}
+
+func (r *Route) toJsonRoute() JsonRoute {
+
+    jstfs := make([]string, len(r.StopTimesFull))
+
+    for i, stf := range r.StopTimesFull {
+        jstfs[i] = stf.DepartureTime
+    }
+
+    return JsonRoute{r.Name, r.TripId, r.RouteType, r.RouteColor, r.RouteTextColor, r.FirstStopName, r.LastStopName, jstfs}
+}
+
+func mergeFlStopNamesByTripIdWithStopRoutes(stops *Stops, flStopNamesByTripId map[int]FirstLastStopNamesByTripId) {
+    for i := range *stops {
+        stop := &(*stops)[i]
+        for j := range stop.Routes {
+            route := &stop.Routes[j]
             route.FirstStopName = flStopNamesByTripId[route.TripId].FirstStopName
             route.LastStopName = flStopNamesByTripId[route.TripId].LastStopName
         }
@@ -185,28 +375,28 @@ func fetchFirstAndLastStopNamesByTripIds(agencyKey string, tripIds []int) map[in
     sem := make(chan bool, 64)
 
     go func() {
-        for tripId := range tripIds {
+        for _, tripId := range tripIds {
 
             sem <- true
 
             go func(tripId int) {
+
                 defer func() { <-sem }()
 
-                sw := stopwatch.Start(0)
+//                sw := stopwatch.Start(0)
 
                 key := fmt.Sprintf("/%s/t/st/fl/%d", agencyKey, tripId)
                 tripPayload := redisClient.Get(key)
                 value := tripPayload.Val()
-                log.Printf("Key: '%s', Value: '%s'", key, value)
 
                 tripFirstLast := make([]string, 2)
 
                 err := json.Unmarshal([]byte(value), &tripFirstLast)
                 if err != nil {
-                    log.Printf("Error: '%s' ...", err.Error())
+                    log.Printf(" * Error: '%s' ...", err.Error())
                 }
 
-                log.Printf("[TRIP][FIND_STOP_TIMES_BY_TRIP_ID] Data Fetch for key: '%s' Done in %d ms", key, sw.ElapsedTime().Nanoseconds() / 1000 / 1000);
+//                log.Printf("[TRIP][FIND_STOP_TIMES_BY_TRIP_ID] Data Fetch for key: '%s' Done in %v", key, sw.ElapsedTime());
 
                 flStopNamesByTripIdChan <- FirstLastStopNamesByTripId{tripId, tripFirstLast[0], tripFirstLast[1]}
             }(tripId)
@@ -228,7 +418,7 @@ func fetchFirstAndLastStopNamesByTripIds(agencyKey string, tripIds []int) map[in
     return flStopNamesByTripIds
 }
 
-func extractTripIds(stops []Stop) []int {
+func extractTripIds(stops Stops) []int {
     tripIdMap := make(map[int]bool)
 
     for _, stop := range stops {
@@ -247,16 +437,16 @@ func extractTripIds(stops []Stop) []int {
     return tripIds
 }
 
-func fetchStopsByDate(agencyKey, date, lat, lon, distance string) []Stop {
+func fetchStopsByDate(agencyKey, date, lat, lon, distance string) Stops {
 
-    query := fmt.Sprintf("select s.stop_id, s.stop_name, 111195 * st_distance(point(%s, %s), s.stop_geo) as stop_distance from gtfs_%s.stops s where 111195 * st_distance(point(%s, %s), s.stop_geo) < %s order by stop_distance asc", lat, lon, agencyKey, lat, lon, distance)
+    query := fmt.Sprintf("select s.stop_id, s.stop_name, s.stop_desc, s.stop_lat, s.stop_lon, s.location_type, 111195 * st_distance(point(%s, %s), s.stop_geo) as stop_distance from gtfs_%s.stops s where 111195 * st_distance(point(%s, %s), s.stop_geo) < %s order by stop_distance asc", lat, lon, agencyKey, lat, lon, distance)
     sw := stopwatch.Start(0)
 
     log.Printf("Query: %s", query)
     rows, err := config.DB.Raw(query).Rows()
     defer rows.Close()
 
-    log.Printf("[STOP_SERVICE][FIND_NEAREST_STOPS] Data Fetch for [agencyKey=%s, date=%s, lat=%s, lon=%s, distance=%s] Done in %d ms", agencyKey, date, lat, lon, distance, sw.ElapsedTime().Nanoseconds() / 1000 / 1000);
+    log.Printf("[STOP_SERVICE][FIND_NEAREST_STOPS] Data Fetch for [agencyKey=%s, date=%s, lat=%s, lon=%s, distance=%s] Done in %v", agencyKey, date, lat, lon, distance, sw.ElapsedTime());
 
     stopChan := make(chan Stop)
 
@@ -266,16 +456,20 @@ func fetchStopsByDate(agencyKey, date, lat, lon, distance string) []Stop {
 
         id := 0
         name := ""
+        desc := ""
+        lat := ""
+        lon := ""
+        locationType := 0
         distance := 0.0
 
         sem := make(chan bool, 512)
 
         for rows.Next() {
-            rows.Scan(&id, &name, &distance)
+            rows.Scan(&id, &name, &desc, &lat, &lon, &locationType, &distance)
 
-            stop := Stop{id, name, distance, nil}
+            stop := Stop{id, name, desc, lat, lon, locationType, distance, nil}
 
-            log.Printf("Stop: %v", stop)
+//            log.Printf("Stop: %v", stop)
 
             sem <- true
 
@@ -284,7 +478,9 @@ func fetchStopsByDate(agencyKey, date, lat, lon, distance string) []Stop {
 
                 stop.Routes = fetchRoutesForDateAndStop(agencyKey, date, stop)
 
-                stopChan <- stop
+                if len(stop.Routes) > 0 {
+                    stopChan <- stop
+                }
 
             }(stop)
         }
@@ -296,7 +492,7 @@ func fetchStopsByDate(agencyKey, date, lat, lon, distance string) []Stop {
         close(stopChan)
     }()
 
-    stops := make([]Stop, 0)
+    stops := make(Stops, 0)
 
     for stop := range stopChan {
         stops = append(stops, stop)
@@ -305,37 +501,42 @@ func fetchStopsByDate(agencyKey, date, lat, lon, distance string) []Stop {
     return stops
 }
 
-func fetchRoutesForDateAndStop(agencyKey, date string, stop Stop) []Route {
-    log.Printf("Fetching routes for stop: %v", stop)
+func fetchRoutesForDateAndStop(agencyKey, date string, stop Stop) Routes {
+//    log.Printf("Fetching routes for stop: %v", stop)
 
     stfs := fetchStopTimesFullForDateAndStop(agencyKey, date, stop)
-
-    groupStopTimesFullByRoute := func (stfs []StopTimeFull) []Route {
-
-        stfsByRouteShortName := make(map[string][]StopTimeFull, 0)
-
-        for _, stf := range stfs {
-            if _, ok := stfsByRouteShortName[stf.RouteShortName]; !ok {
-                stfsByRouteShortName[stf.RouteShortName] = make([]StopTimeFull, 0)
-            }
-
-            stfsByRouteShortName[stf.RouteShortName] = append(stfsByRouteShortName[stf.RouteShortName], stf)
-        }
-
-        routes := make([]Route, len(stfsByRouteShortName))
-
-        for rsn, stfs := range stfsByRouteShortName {
-            routes = append(routes, Route{rsn, 0, "", "", stfs})
-        }
-
-        return routes
-    }
 
     return groupStopTimesFullByRoute(stfs)
 }
 
+func groupStopTimesFullByRoute(stfs []StopTimeFull) Routes {
+
+    stfsByRouteShortName := make(map[string][]StopTimeFull, 0)
+
+    for _, stf := range stfs {
+        if _, ok := stfsByRouteShortName[stf.RouteShortName]; !ok {
+            stfsByRouteShortName[stf.RouteShortName] = make([]StopTimeFull, 0)
+        }
+
+        stfsByRouteShortName[stf.RouteShortName] = append(stfsByRouteShortName[stf.RouteShortName], stf)
+    }
+
+    routes := make(Routes, 0)
+
+    for rsn, stfs := range stfsByRouteShortName {
+        if len(stfs) > 0 {
+
+            sort.Sort(StopTimeFullByDepartureDate(stfs))
+
+            routes = append(routes, Route{rsn, stfs[0].TripId, stfs[0].RouteType, stfs[0].RouteColor, stfs[0].RouteTextColor, "", "", stfs})
+        }
+    }
+
+    return routes
+}
+
 func fetchStopTimesFullForDateAndStop(agencyKey, date string, stop Stop) []StopTimeFull {
-    log.Printf("Fetching stop times full for date: %s & stop: %v", date, stop)
+//    log.Printf("Fetching stop times full for date: %s & stop: %v", date, stop)
 
     day, _ := time.Parse("2006-01-02", date)
     dayOfWeek := day.Weekday().String()
@@ -346,14 +547,14 @@ func fetchStopTimesFullForDateAndStop(agencyKey, date string, stop Stop) []StopT
         var wg sync.WaitGroup
         wg.Add(2)
 
-        sw := stopwatch.Start(0)
+//        sw := stopwatch.Start(0)
 
         go fetchStopTimesFullForCalendar(agencyKey, stop, date, dayOfWeek, stfChan, &wg)
         go fetchStopTimesFullForCalendarDates(agencyKey, stop, date, stfChan, &wg)
 
         wg.Wait()
 
-        log.Printf("[STOP_TIMES_FULL][FIND_LINES_BY_STOP_ID_AND_DATE] Data Fetch done in %d ms", sw.ElapsedTime().Nanoseconds() / 1000 / 1000);
+//        log.Printf("[STOP_TIMES_FULL][FIND_LINES_BY_STOP_ID_AND_DATE] Data Fetch done in %v", sw.ElapsedTime());
 
         close(stfChan)
     }()
@@ -361,7 +562,9 @@ func fetchStopTimesFullForDateAndStop(agencyKey, date string, stop Stop) []StopT
     stfs := make([]StopTimeFull, 0)
 
     for stf := range stfChan {
-        stfs = append(stfs, stf)
+        if (len(stfs) < 5) {
+            stfs = append(stfs, stf)
+        }
     }
 
     return stfs
@@ -391,12 +594,10 @@ func fetchStopTimesFullForCalendar(agencyKey string, stop Stop, date, dayOfWeek 
         " where " +
         "    stf.stop_id=%d and" +
         "    c.start_date <= '%s' and" +
-        "    c.end_date >= '%s' and %s=1 and" +
-        "    stf.departure_time > '%s'" +
-        "    order by stf.departure_time asc" +
-        "    limit %d", agencyKey, agencyKey, stop.Id, date, date, dayOfWeek, time.Now().Format("15:04:05"), 5)
+        "    c.end_date >= '%s' and %s=1", agencyKey, agencyKey, stop.Id, date, date, dayOfWeek)
+//        "    c.end_date >= '%s' and %s=1", agencyKey, agencyKey, stop.Id, date, date, dayOfWeek/*, time.Now().Format("15:04:05"), 5*/)
 
-    log.Printf("Query calendar: %s", queryCalendar)
+//    log.Printf("Query calendar: %s", queryCalendar)
 
     calendarRows, err := config.DB.Raw(queryCalendar).Rows()
 
@@ -445,12 +646,10 @@ func fetchStopTimesFullForCalendarDates(agencyKey string, stop Stop, date string
         "    gtfs_%s.stop_times_full stf inner join" +
         "    gtfs_%s.calendar_dates cd on stf.service_id=cd.service_id" +
         " where" +
-        "    stf.stop_id=%d and cd.date = '%s' and" +
-        "    stf.departure_time > '%s'" +
-        "    order by stf.departure_time asc" +
-        "    limit %d", agencyKey, agencyKey, stop.Id, date, time.Now().Format("15:04:05"), 5)
+        "    stf.stop_id=%d and cd.date = '%s'", agencyKey, agencyKey, stop.Id, date)
+//        "    stf.stop_id=%d and cd.date = '%s'", agencyKey, agencyKey, stop.Id, date/*, time.Now().Format("15:04:05"), 5*/)
 
-    log.Printf("Query calendar dates : %s", queryCalendarDate)
+//    log.Printf("Query calendar dates : %s", queryCalendarDate)
 
     calendarDateRows, _ := config.DB.Raw(queryCalendarDate).Rows()
 
@@ -471,4 +670,42 @@ func fetchStopTimesFullForCalendarDates(agencyKey string, stop Stop, date string
         stfChan <- StopTimeFull{stopId, stopName, stopDesc, stopLat, stopLon, locationType, arrivalTime, departureTime, stopSequence, directionId, routeShortName, routeType, routeColor, routeTextColor, tripId}
     }
 
+}
+
+
+func fetchNearestAgencies(lat, lon string) Agencies {
+
+    query := fmt.Sprintf("select agency_key, agency_id, agency_name, agency_url, agency_timezone, agency_lang, agency_min_lat, agency_max_lat, agency_min_lon, agency_max_lon from agencies where agency_min_lat <= %s and agency_max_lat >= %s and agency_min_lon <= %s and agency_max_lon >= %s", lat, lat, lon, lon)
+    sw := stopwatch.Start(0)
+
+    log.Printf("Query: %s", query)
+    rows, err := config.DB.Raw(query).Rows()
+    defer rows.Close()
+
+    log.Printf("[STOP_SERVICE][FIND_NEAREST_AGENCIES] Data Fetch for [lat=%s, lon=%s] Done in %v", lat, lon, sw.ElapsedTime());
+
+    utils.FailOnError(err, "Failed to execute query")
+
+    key := ""
+    id := 0
+    name := ""
+    url := ""
+    timezone := ""
+    lang := ""
+    minLat := 0.0
+    maxLat := 0.0
+    minLon := 0.0
+    maxLon := 0.0
+
+    agencies := make([]Agency, 0)
+
+    for rows.Next() {
+        rows.Scan(&key, &id, &name, &url, &timezone, &lang, &minLat, &maxLat, &minLon, &maxLon)
+
+        agency := Agency{key, id, name, url, timezone, lang, minLat, maxLat, minLon, maxLon}
+
+        agencies = append(agencies, agency)
+    }
+
+    return agencies
 }
